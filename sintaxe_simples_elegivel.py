684ff47a246d076749e1c7ed108dc07ca8c0ed1e
Ana Caroline <lealanacaroline00@gmail.com>
2025-04-12 18:22:54 -0300

    Add files via upload

---
+++ sintaxe_simples_elegivel.py
@@ -0,0 +1,9 @@
+#Exemplo de código com sintaxe simples e elegível
+#Definindo uma função que calcule a soma de dois números
+
+def soma(a, b):
+    return (a + b)
+#Usando a fun~~ao para chamar dois números
+resultado = soma(3, 5)
+#Exibindo o resultado
+print("A soma é: ", resultado)
