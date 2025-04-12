ccc8630a0da31024a6b23451ac28f5493cce67f1
Ana Caroline <lealanacaroline00@gmail.com>
2025-04-12 18:34:45 -0300

    Add files via upload

---
+++ biblioteca_padrao_modulomath.py
@@ -0,0 +1,6 @@
+#Exemplo de uso da biblioteca padrão do Python (módulo math)
+import math
+#Calculando a raiz quadrada de um número
+numero = 16
+raiz_quadrada = math.sqrt(numero)
+print("A raiz quadrada de ", numero, "é: ", raiz_quadrada)
