d0644135a1f1e06e968fabd6e3375398b6e82a22
Ana Caroline <lealanacaroline00@gmail.com>
2025-04-12 07:22:20 -0300

    Add files via upload

---
+++ declarando_e_exibindo_valores_variaveis.py
@@ -0,0 +1,19 @@
+#Declarando variáveis
+
+nome = "João"     #Tipo de dado: string 
+
+idade = 25        #Tipo de dado: inteiro
+
+altura = 1.75     #Tipo de dado: float
+
+is_estudante = True  #Tipo de dado: booleano
+
+#Exibindo os valores das variáveis (funções)
+
+print("nome: ", nome)
+
+print("Idade: ", idade)
+
+print("Altura: ", altura)
+
+print ("Estudante: ", is_estudante)
